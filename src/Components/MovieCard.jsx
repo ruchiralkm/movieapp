a207b338c8b991f9ddef40294945037832f86ddb
Ruchira Kaluarachchi <151660406+ruchiralkm@users.noreply.github.com>
2024-08-11 12:55:17 +0530

    Add files via upload

---
+++ src/Components/MovieCard.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function MovieCard({ movie: { imdbID, Year, Poster, Title, Type } }) {
+  return (
+    <>
+      <div className="movie" key={imdbID}>
+        <div>
+          <p>{Year}</p>
+        </div>
+        <div>
+          <img
+            src={
+              Poster !== "N/A"
+                ? Poster
+                : "https://th.bing.com/th/id/R.03e713c84818c99d0285a2eb49a33d52?rik=J7JMHNZXqBdJjw&riu=http%3a%2f%2fwww.fubiz.net%2fwp-content%2fuploads%2f2015%2f03%2fForrest-Gump.jpg&ehk=FhWxY24g1daJzM2xwBeTHURDp%2fcxAHNFHumkwsppbO4%3d&risl=&pid=ImgRaw&r=0"
+            }
+            alt={Title}
+          />
+        </div>
+
+        <div>
+          <span>{Type}</span>
+          <h3>{Title}</h3>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default MovieCard;
